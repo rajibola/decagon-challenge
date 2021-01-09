595c30a0e0e80995b9991f1d1b7a15634b5dc59d
Ridwan Ajibola <rajibola1997@gmail.com>
2021-01-09 23:35:33 +0100

    feat: Set up file system

--- src/screens/home-screen/index.js
+++ src/screens/home-screen/index.js
@@ -1,9 +1,12 @@
+import {parse, Parser} from 'papaparse';
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {styles} from './styles';
+import RNFS from 'react-native-fs';
 
 export class HomeScreen extends Component {
   render() {
+    // console.log(results);
     return (
       <View style={styles.container}>
         <Text> HomeScreen </Text>
